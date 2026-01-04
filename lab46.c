31bcfcd0c7219d05c78052567ec0934d2bed14cc
6704062616214 <s6704062616214@email.kmutnb.ac.th>
2026-01-04 18:32:00 +0700

    lab4 item 6

---
+++ lab46.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+int i,j,a,r;
+	scanf ("%d %d", &a,&r);
+		for (i = 0; i<r; i++){
+				for (j = 0; j<r; j++){
+					printf("%d",a);
+				}
+			printf("\n");
+				}
+return 0;
+} 
